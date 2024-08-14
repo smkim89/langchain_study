5c47a94c6884caae30c2bb721f3f5c3959383491
김성민 <kiak357@gmail.com>
2024-08-14 18:52:57 +0900

    streamlit 기본 사용법 study.

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,41 +1,59 @@
 import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
 import streamlit as st
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SERVICE_KEY = os.getenv('OPEN_API_KEY')
+openai_api_key= SERVICE_KEY
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📃",
 )
 
+
+
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings(openai_api_key=SERVICE_KEY)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
 st.title("DocumentGPT")
 
-# streamlit은 행위가 작동 (input message) 이되면 화면을 무조건 다시 reload 하기때문에 앞에 저장되어있는 message 가 없음.
-# session state는 재실행해도 data가 보존이 됌.
-# 계속 reload하기때문에 조건절을 추가하여 배열로 초기화함.
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
 
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
-    )
-
-
-message = st.chat_input("Send a message to the ai ")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")

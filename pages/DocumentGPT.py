9cd87c3d3d13a872c5317f2c5ab2cecf639a25ab
김성민 <kiak357@gmail.com>
2024-08-14 19:21:32 +0900

    DocumentGpt UI 완료

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 
-
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -39,6 +39,22 @@ def embed_file(file):
     return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -46,14 +62,23 @@ st.markdown(
 Welcome!
 
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+else:
+    st.session_state["messages"] = []
